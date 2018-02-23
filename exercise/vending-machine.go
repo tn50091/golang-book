262fe32d2fd36d6559083a8a32ea6a4713d0e6c9
tn50091 <suriya.du@tnis.com>
2018-02-23 18:17:01 +0700

    Update vending-machine.go

--- exercise/vending-machine.go
+++ exercise/vending-machine.go
@@ -20,19 +20,62 @@ func (v NewVendingMachine) GetInsertedMoney() int{
 	return v.coins
 }
 
-func (v NewVendingMachine) SelectCC() int {
-	//v.items = "CC"
-	//if v.coins > 12 {return v.items,v.coins-12}
-	//return v.items,v.coins
-	if v.coins > 12 {return v.coins-12}
-	return v.coins
+func (v *NewVendingMachine) SelectCC() string {
+	v.items = "CC"
+	cash := v.coins-12
+	output := v.items
+	return GenOutput(cash, output)
+}
+
+func (v *NewVendingMachine) SelectSD() string {
+	v.items = "SD"
+	cash := v.coins-18
+	output := v.items
+	return GenOutput(cash, output)
+}
+
+func (v *NewVendingMachine) CoinReturn() string {
+	v.items = ""
+	cash := v.coins-0
+	output := v.items
+	return GenOutput(cash, output)
+}
+
+func GenOutput (cash int, output string) string {
+	for cash>0 {
+		if cash >= 10 {
+			cash -= 10
+			output += ",T"
+			continue
+		}
+		if cash >= 5 {
+			cash -= 5
+			output += ",F"
+			continue
+		}
+		if cash >= 2 {
+			cash -= 2
+			output += ",TW"
+			continue
+		}
+		if cash >= 1 {
+			cash -= 1
+			output += ",O"
+			continue
+		}
+	}
+	return output
 }
 
 func main() {
 	vm := NewVendingMachine{0,""}
 	vm.InsertCoin("T")
-	vm.InsertCoin("T")
-	fmt.Println("Inserted Money:", vm.GetInsertedMoney()) // 20
-	can := vm.SelectCC()
-	fmt.Println(can) // CC, F, TW, O
+	vm.InsertCoin("F")
+	vm.InsertCoin("TW")
+	vm.InsertCoin("O")
+	fmt.Println("Inserted Money:", vm.GetInsertedMoney())
+	//can := vm.SelectCC()
+	//can := vm.SelectSD()
+	can := vm.CoinReturn()
+	fmt.Println("Item,Change:", can)
 }
