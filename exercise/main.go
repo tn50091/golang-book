40c1f67a7347fd3a69c0e0e1fc1eea32499e9f75
tn50091 <suriya.du@tnis.com>
2018-02-26 10:39:51 +0700

    Update Main.go

--- exercise/main.go
+++ exercise/main.go
@@ -6,6 +6,7 @@ import (
 
 type VendingMachine struct{
 	insertedMoney int
+	coins map[string]int
 }
 
 func (v VendingMachine) InsertedMoney() int {
@@ -13,12 +14,19 @@ func (v VendingMachine) InsertedMoney() int {
 }
 
 func (v *VendingMachine) InsertCoin(coin string) {
-	v.insertedMoney = 10
+	//v.insertedMoney = 10
+	if coin == "T" {v.insertedMoney += 10}
+	if coin == "F"  {v.insertedMoney += 5}
+	if coin == "TW"  {v.insertedMoney += 2}
+	if coin == "O"  {v.insertedMoney += 1}
 }
 
 func main() {
 	vm := VendingMachine{}
 	fmt.Println("Inserted Money:", vm.InsertedMoney())	// Inserted Money: 0
 	vm.InsertCoin("T")
+	vm.InsertCoin("F")
+	vm.InsertCoin("TW")
+	vm.InsertCoin("O")
 	fmt.Println("Inserted Money:", vm.InsertedMoney())	// Inserted Money: 10
 }
