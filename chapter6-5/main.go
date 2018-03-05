0c4a48a194a265617dffedfd996c200a621a7510
tn50091 <suriya.du@tnis.com>
2018-03-05 11:28:26 +0700

    Add chapter 12 and update chapter6-5

--- chapter6-5/main.go
+++ chapter6-5/main.go
@@ -4,7 +4,7 @@ import (
 	"strconv"
 	"fmt"
 )
-
+/*
 func main() {
 	for i := 1; i <= 15; i++ {
 		result := FizzBuzz(i)
@@ -21,4 +21,22 @@ func FizzBuzz(i int) string {
 		}
 	}
 	return strconv.Itoa(i)
+}
+*/
+
+func main() {
+	var FizzBuzz func (int) string
+	for i := 1; i <= 15; i++ {
+		FizzBuzz = func(i int) string {
+			ln := [3]int{15,3,5}
+			ret := [3]string{"FizzBuzz","Fizz","Buzz"}
+			for j:=0;j<len(ln);j++ {
+				if i%ln[j] == 0 {
+					return ret[j]
+				}
+			}
+			return strconv.Itoa(i)
+		}
+		fmt.Println(i,FizzBuzz(i))
+	}
 }
